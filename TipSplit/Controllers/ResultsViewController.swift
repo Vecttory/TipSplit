6fe8dbda7edc239537e895b35be2c70d253cbc91
Alberto Ola <alberto.ola.dev@gmail.com>
2022-10-25 21:56:39 -0600

    chore: project renaming and IBActions and IBOutlets added to the ResultsViewController

---
+++ TipSplit/Controllers/ResultsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ResultsViewController.swift
+//  TipSplit
+//
+//  Created by Alberto Ola on 25/10/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+}
